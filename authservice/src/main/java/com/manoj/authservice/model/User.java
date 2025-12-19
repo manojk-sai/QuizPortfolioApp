61b177c44e616a6482a91b522f34d28f0aea9764
manojk-sai <manojsaikothapati@gmail.com>
2025-12-18 18:56:41 -0600

    Added email validation

--- authservice/src/main/java/com/manoj/authservice/model/User.java
+++ authservice/src/main/java/com/manoj/authservice/model/User.java
@@ -1,6 +1,8 @@
 package com.manoj.authservice.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
@@ -10,6 +12,8 @@ public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be a valid format")
     @Column(unique = true, nullable = false)
     private String email;
 
